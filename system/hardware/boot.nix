1863413122c40124c34c73923733d6d62f0d4e8e
mad <mad@example.com>
2026-04-11 17:30:47 +0200

    .

--- system/hardware/boot.nix
+++ system/hardware/boot.nix
@@ -2,8 +2,7 @@
 
 {
 	boot.loader.systemd-boot.enable = true;
-	boot.loader.systemd-boot.configurationLimit = 10;
-	boot.loader.systemd-boot.timeout = 3;
+
 	boot.loader.efi.canTouchEfiVariables = true;
 
 	boot.plymouth.enable = true;
