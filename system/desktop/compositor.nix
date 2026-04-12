f1826fb8d822b5c0b438cd4c021f972a3c387c90
mad <mad@example.com>
2026-04-12 17:58:03 +0200

    .

--- system/desktop/compositor.nix
+++ system/desktop/compositor.nix
@@ -2,11 +2,7 @@
 
 {
 	services.displayManager.ly.enable = true;
-	programs.niri = {
-		enable = true;
-		package = pkgs.niri-unstable;
-	};
-
+	
 	# Disable SSH agent auth and U2F for the TTY display manager
 	security.pam.services.ly = {
 		sshAgentAuth = false;
