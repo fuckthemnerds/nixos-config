85a13b3c9ee2edc17ebb6dd5395c8e5b2a5b89cc
mad <mad@example.com>
2026-04-14 00:38:39 +0200

    .

--- modules/apps/defaults.nix
+++ modules/apps/defaults.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, globals, ... }:
 let
 	dirContents = builtins.readDir ./.;
 	appFiles = lib.filterAttrs (name: type:
@@ -14,54 +14,56 @@ in
 		enable = lib.mkDefault true;
 	});
 
-	xdg = {
-		mimeApps = {
-			enable = true;
-			defaultApplications = {
-				"text/plain"                  = "nvim.desktop";
-				"text/x-shellscript"          = "nvim.desktop";
-				"application/pdf"             = "org.pwmt.zathura.desktop";
+	home-manager.users.${globals.userName} = {
+		xdg = {
+			mimeApps = {
+				enable = true;
+				defaultApplications = {
+					"text/plain"                  = "nvim.desktop";
+					"text/x-shellscript"          = "nvim.desktop";
+					"application/pdf"             = "org.pwmt.zathura.desktop";
 
-				"text/html"                   = "librewolf.desktop";
-				"x-scheme-handler/http"       = "librewolf.desktop";
-				"x-scheme-handler/https"      = "librewolf.desktop";
-				"x-scheme-handler/about"      = "librewolf.desktop";
-				"x-scheme-handler/unknown"    = "librewolf.desktop";
+					"text/html"                   = "librewolf.desktop";
+					"x-scheme-handler/http"       = "librewolf.desktop";
+					"x-scheme-handler/https"      = "librewolf.desktop";
+					"x-scheme-handler/about"      = "librewolf.desktop";
+					"x-scheme-handler/unknown"    = "librewolf.desktop";
 
-				"image/png"                   = "imv.desktop";
-				"image/jpeg"                  = "imv.desktop";
-				"image/gif"                   = "imv.desktop";
-				"image/webp"                  = "imv.desktop";
-				"image/svg+xml"               = "imv.desktop";
-				"video/mp4"                   = "mpv.desktop";
-				"video/webm"                  = "mpv.desktop";
-				"video/mkv"                   = "mpv.desktop";
+					"image/png"                   = "imv.desktop";
+					"image/jpeg"                  = "imv.desktop";
+					"image/gif"                   = "imv.desktop";
+					"image/webp"                  = "imv.desktop";
+					"image/svg+xml"               = "imv.desktop";
+					"video/mp4"                   = "mpv.desktop";
+					"video/webm"                  = "mpv.desktop";
+					"video/mkv"                   = "mpv.desktop";
 
-				"application/zip"             = "org.gnome.FileRoller.desktop";
-				"application/x-tar"           = "org.gnome.FileRoller.desktop";
+					"application/zip"             = "org.gnome.FileRoller.desktop";
+					"application/x-tar"           = "org.gnome.FileRoller.desktop";
+				};
+			};
+
+			userDirs = {
+				enable              = true;
+				setSessionVariables = true;
+				createDirectories   = true;
+				download    = "$HOME/Downloads";
+				documents   = "$HOME/Documents";
+				music       = "$HOME/Music";
+				pictures    = "$HOME/Pictures";
+				videos      = "$HOME/Videos";
+				desktop     = "$HOME";
+				publicShare = "$HOME";
+				templates   = "$HOME";
 			};
 		};
 
-		userDirs = {
-			enable              = true;
-			setSessionVariables = true;
-			createDirectories   = true;
-			download    = "$HOME/Downloads";
-			documents   = "$HOME/Documents";
-			music       = "$HOME/Music";
-			pictures    = "$HOME/Pictures";
-			videos      = "$HOME/Videos";
-			desktop     = "$HOME";
-			publicShare = "$HOME";
-			templates   = "$HOME";
+		home.sessionVariables = {
+			EDITOR   = "nvim";
+			VISUAL   = "nvim";
+			MANPAGER = "nvim +Man!";
+			PAGER    = "bat --style=plain";
+			SSH_AUTH_SOCK = "\${XDG_RUNTIME_DIR}/app/org.keepassxc.KeePassXC/ssh-agent.socket";
 		};
-	};
-
-	home.sessionVariables = {
-		EDITOR   = "nvim";
-		VISUAL   = "nvim";
-		MANPAGER = "nvim +Man!";
-		PAGER    = "bat --style=plain";
-		SSH_AUTH_SOCK = "\${XDG_RUNTIME_DIR}/app/org.keepassxc.KeePassXC/ssh-agent.socket";
 	};
 }
